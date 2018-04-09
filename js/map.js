eba46d29ef954bc82c38ec84fe62ed91d8aa4cfc
EugeneKat1009 <zhenkatusha@gmail.com>
2018-04-09 15:49:04 +0300

    Добавил features

--- js/map.js
+++ js/map.js
@@ -1,18 +1,18 @@
 'use strict';
 // Переменные
-var OFFER_TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var OFFER_CHECKIN = ['12:00', '13:00', '14:00'];
-var OFFER_CHECKOUT = ['12:00', '13:00', '14:00'];
-var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var offerTitle = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var offerType = ['palace', 'flat', 'house', 'bungalo'];
+var offerCheckin = ['12:00', '13:00', '14:00'];
+var offerCheckout = ['12:00', '13:00', '14:00'];
+var offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var offerPriceMin = 1000;
 var offerPriceMax = 1000000;
 var offerRoomsMin = 1;
 var offerRoomsMax = 5;
 var guestsMin = 1;
 var guestsMax = 14;
-var OFFER_FEATURES_MIN = 0;
-var OFFER_FEATURES_MAX = OFFER_FEATURES.length;
+var offerFeaturesMin = 0;
+var offerFeaturesMax = offerFeatures.length;
 var locationMinX = 300;
 var locationMaxX = 900;
 var locationMinY = 150;
@@ -51,15 +51,15 @@ var createMap = function () {
       avatar: 'img/avatars/user0' + getRandomNumber(1, 8) + '.png'
     },
     offer: {
-      title: OFFER_TITLE[getRandom(OFFER_TITLE.length)],
-      addres: locationX + ', ' + locationY,
+      title: offerTitle[getRandom(offerTitle.length)],
+      address: locationX + ', ' + locationY,
       price: getRandomNumber(offerPriceMin, offerPriceMax),
-      type: OFFER_TYPE[getRandom(OFFER_TYPE.length)],
+      type: offerType[getRandom(offerType.length)],
       rooms: getRandomNumber(offerRoomsMin, offerRoomsMax),
       guests: getRandomNumber(guestsMin, guestsMax),
-      checkin: OFFER_CHECKIN[getRandom(OFFER_CHECKIN.length)],
-      checkout: OFFER_CHECKOUT[getRandom(OFFER_CHECKOUT.length)],
-      features: OFFER_FEATURES.slice(getRandomNumber(OFFER_FEATURES_MIN, OFFER_FEATURES_MAX)),
+      checkin: offerCheckin[getRandom(offerCheckin.length)],
+      checkout: offerCheckout[getRandom(offerCheckout.length)],
+      features: offerFeatures.slice(getRandomNumber(offerFeaturesMin, offerFeaturesMax)),
       description: '',
       photos: 'http://o0.github.io/assets/images/tokyo/hotel1.jpg'
     },
@@ -81,38 +81,48 @@ var renderButtonMap = function (btn) {
   return button;
 };
 
-for (var i = 1; i <= 8; i++) {
+for (var i = 0; i <= 8; i++) {
   mapObjects.push(createMap());
 }
 
 // кнопки
-for (i = 0; i < 8; i++) {
+for (i = 0; i <= 8; i++) {
   fragment.appendChild(renderButtonMap(mapObjects));
 }
 buttonElement.appendChild(fragment);
 
 // функция генерации объявления
-var articleRender = function (article) {
+var articleRender = function (articles) {
+
   var advert = articleTemplate.cloneNode(true);
-  advert.querySelector('.popup__title').textContent = article[i].offer.title;
-  // advert.querySelector('.popup__text--address').textContent = article[i].offer.address;
-  advert.querySelector('.popup__text--price').textContent = article[i].offer.price + '₽/ночь';
+  advert.querySelector('.popup__title').textContent = articles[i].offer.title;
+  advert.querySelector('.popup__text--address').innerHTML = articles[i].offer.address;
+  advert.querySelector('.popup__text--price').textContent = articles[i].offer.price + '₽/ночь';
 
   var accommodationType;
-  if (article[i].offer.type === 'flat') {
+  if (articles[i].offer.type === 'flat') {
     accommodationType = 'Квартира';
-  } else if (article[i].offer.type === 'bungalo') {
+  } else if (articles[i].offer.type === 'bungalo') {
     accommodationType = 'Бунгало';
-  } else if (article[i].offer.type === 'house') {
+  } else if (articles[i].offer.type === 'house') {
     accommodationType = 'Дом';
   }
 
   advert.querySelector('.popup__type').textContent = accommodationType;
-  advert.querySelector('.popup__text--capacity').textContent = article[i].offer.rooms + ' комнаты для ' + article[i].offer.rooms + ' гостей';
-  advert.querySelector('.popup__text--time').textContent = 'Заезд после ' + article[i].offer.checkin + ', выезд до ' + article[i].offer.checkout;
-  advert.querySelector('.popup__photos').src = article[i].offer.photos;
-  advert.querySelector('.popup__feature').textContent = article[i].offer.features;
-  advert.querySelector('.popup__avatar').src = article[i].author.avatar;
+  advert.querySelector('.popup__text--capacity').textContent = articles[i].offer.rooms + ' комнаты для ' + articles[i].offer.rooms + ' гостей';
+  advert.querySelector('.popup__text--time').textContent = 'Заезд после ' + articles[i].offer.checkin + ', выезд до ' + articles[i].offer.checkout;
+  advert.querySelector('.popup__photos').src = articles[i].offer.photos;
+  // advert.querySelectorAll('.popup__features').innerHTML = articles[i].offer.features;
+
+  var featuresItems = advert.querySelector('.popup__features');
+  featuresItems.innerHTML = '';
+  for (var j = 0; j < articles[i].offer.features.length; j++) {
+    var featuresItem = document.createElement('li');
+    featuresItem.classList.add('popup__feature', 'popup__feature--' + articles[i].offer.features[j]);
+    featuresItems.appendChild(featuresItem);
+  }
+
+  advert.querySelector('.popup__avatar').src = articles[i].author.avatar;
 
   return advert;
 };
